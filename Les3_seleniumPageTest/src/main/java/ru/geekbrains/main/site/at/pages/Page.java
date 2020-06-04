4fb5ef41f576c608016ee1d6e01addbc172ce99d
Olesya Rodionova <rodionova-o@yandex.ru>
2020-06-04 15:33:58 +0300

    Page.java is modificated

--- Les3_seleniumPageTest/src/main/java/ru/geekbrains/main/site/at/pages/Page.java
+++ Les3_seleniumPageTest/src/main/java/ru/geekbrains/main/site/at/pages/Page.java
@@ -2,9 +2,14 @@ package ru.geekbrains.main.site.at.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Page extends BasePage {
+
+    @FindBy(className = "gb-header__title")
+    private WebElement pageHeader;
 
     public static By headerSelector = By.className("gb-header__title");
 
@@ -18,12 +23,12 @@ public class Page extends BasePage {
     }
 
     public Page checkHeader(String expected) {
-        assertEquals(expected, driver.findElement(headerSelector)
-                .getText());
+        assertEquals(expected, pageHeader.getText());
         return this;
     }
 
     public NavigationTab getNavigationTab() {
+
         return navigationTab;
     }
 }
